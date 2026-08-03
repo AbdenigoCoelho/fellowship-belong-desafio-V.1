c09ff6095842f69c34e09d750d25ebb7a257993a
AbdenigoCoelho <abdeniotel@gmail.com>
2026-08-03 05:34:40 -0400

    feat: register RatingsModule in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,10 @@ import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { MoviesModule } from './movies/movies.module';
 import { MovieListModule } from './movie-list/movie-list.module';
+import { RatingsModule } from './ratings/ratings.module';
 
 @Module({
-  imports: [PrismaModule, AuthModule, UsersModule, MoviesModule, MovieListModule],
+  imports: [PrismaModule, AuthModule, UsersModule, MoviesModule, MovieListModule, RatingsModule],
   controllers: [AppController],
   providers: [AppService],
 })
